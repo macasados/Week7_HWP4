5e5dfc462ac6516d112e8c83eff981d2a40ea200
macasados <112190950+macasados@users.noreply.github.com>
2022-10-13 02:44:41 +0000

    First submission

--- .ccls-cache/@home@runner@Week7HWP4/main.cpp
+++ .ccls-cache/@home@runner@Week7HWP4/main.cpp
@@ -3,6 +3,7 @@
 using namespace std;
 
 void fillArray(int list[], int listSize);
+int indexSmallestElement(const int list [], int listSize);
 
 const int Array_Size = 5;
   
@@ -12,20 +13,13 @@ int main() {
   
   fillArray(listA, Array_Size);
 
-  
- 
+ int indSmall = indexSmallestElement(listA, Array_Size);
+
+  cout << indexSmallestElement(listA, Array_Size) << endl;
 }
 
 
-
-
-
-
-
-
 void fillArray(int list[], int listSize){
-
-  cout << "Enter " << listSize << " integers: ";
 
   int index;
 
@@ -37,11 +31,11 @@ void fillArray(int list[], int listSize){
   return;
 }
 
-int indexLargestElement(const int list [], int listSize){
-int maxIndex = 0; //default assumption is to assume the largest value is in the largest spot
+int indexSmallestElement(const int list [], int listSize){
+int maxIndex = 0;
 
     for (int index = 1; index < listSize; index++){
-      if (list[maxIndex] < list[index])
+      if (list[maxIndex] > list[index])
         maxIndex = index;
     }
 
