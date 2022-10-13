3b18dbdb617c2418566f035a8f0544d53b83e110
macasados <112190950+macasados@users.noreply.github.com>
2022-10-13 03:39:12 +0000

    Trying to create a dynamic array

--- .ccls-cache/@home@runner@Week7HWP4/main.cpp
+++ .ccls-cache/@home@runner@Week7HWP4/main.cpp
@@ -6,20 +6,27 @@ void fillArray(int list[], int listSize);
 int indexSmallestElement(const int list [], int listSize);
 
 const int Array_Size = 5;
-  
+
 int main() {
 
+  int n, x;
+  cin >> n;
+
+  int listB[n] = {0};
+  
+  
   int listA[Array_Size] = {0};
   
   fillArray(listA, Array_Size);
 
- int indSmall = indexSmallestElement(listA, Array_Size);
+  indexSmallestElement(listA, Array_Size);
 
   cout << indexSmallestElement(listA, Array_Size) << endl;
 }
 
-
 void fillArray(int list[], int listSize){
+
+  cout << "Enter " << listSize << " integers: ";
 
   int index;
 
@@ -32,7 +39,7 @@ void fillArray(int list[], int listSize){
 }
 
 int indexSmallestElement(const int list [], int listSize){
-int maxIndex = 0;
+int maxIndex = 0; //default assumption is to assume the largest value is in the largest spot
 
     for (int index = 1; index < listSize; index++){
       if (list[maxIndex] > list[index])
